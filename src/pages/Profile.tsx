e1a00aebead559d05d241e565351d9b30e760113
amandaalexandre <amandaalexandre@outlook.com>
2024-05-24 06:27:45 -0300

    feat: pull nickname from DB

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,17 +1,40 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { CaretRight, PencilSimple, User, UserCircle } from '@phosphor-icons/react';
 import BlackCard from '../components/BlackCard';
 import { useNavigate } from 'react-router-dom';
 import '../styles/profile.css';
+import getUserData from '../utils/getUserData';
 
 const Profile: React.FC = () => {
+    const user_id = 1;
     const navigate = useNavigate();
+    const [nickname, setNickname] = useState<string>('')
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                // Fetch user data from database
+                const userData = await getUserData(user_id);
+                if (userData.length > 0) {
+                    const nicknameData = userData[0].nickname;
+                    setNickname(nicknameData);
+                    console.log(nicknameData);
+                    // Insert handler function here
+                }
+            } catch (error) {
+                console.error('Error fetching user data:', error);
+            }
+        };
+    
+        fetchData();
+    }, []);
+    
 
     return (
         <main className='profile'>
             <span className='profile-pic-title'>
                 <UserCircle size={48}/>
-                <p className='profile-nickname'>Nickname</p>
+                <p className='profile-nickname'>{nickname}</p>
                 {/* Insert edit profile link */}
                 <PencilSimple size={24} weight="fill" className='icon-clickable' onClick={() => navigate('/user-settings/:user_id')}/>
             </span>
