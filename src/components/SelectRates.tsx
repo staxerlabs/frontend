b42d7472768dd95555115676fcda139e401f17d5
amandaalexandre <amandaalexandre@outlook.com>
2024-05-24 02:54:15 -0300

    feat: add select rates percentages

--- src/components/SelectRates.tsx
+++ src/components/SelectRates.tsx
@@ -1,15 +1,45 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { useNavigate } from 'react-router-dom';
 import '../styles/onboarding.css'
 import Card from './CardText';
+import supabase from '../utils/supabase';
 
 interface SelectRatesProps {}
 
 const SelectRates: React.FC<SelectRatesProps> = () => {
+
+    // get user id
+    const user_id = 1;
+
     const navigate = useNavigate();
 
-  const SelectRatesClickHandler = () => {
-    navigate('/newsafe')
+    const [trade, setTrade] = useState<boolean>(false);
+    const [tradeRate, setTradeRate] = useState<number>(0);
+
+    const [income, setIncome] = useState<boolean>(false);
+    const [incomeRate, setIncomeRate] = useState<number>(0);
+
+    const [donation, setDonation] = useState<boolean>(false);
+    const [donationRate, setDonationRate] = useState<number>(0);
+
+  const SelectRatesClickHandler = async () => {  
+    // Send percentage info to the backend
+    const { data, error } = await supabase
+    .from('user_rates')
+    .insert([
+      { id_user: user_id, 
+        trade_rate: trade ? tradeRate : null, 
+        income_rate: income ? incomeRate : null, 
+        donation_rate: donation ? donationRate : null},
+    ])
+    .select()
+
+    if (error) {
+      console.error('Error inserting rates:', error)
+    } else {
+      navigate('/newsafe')
+    }
+
   }
 
   return (
@@ -21,17 +51,17 @@ const SelectRates: React.FC<SelectRatesProps> = () => {
         <div className="rates-grid">
             <span className='rates-headline'>tax rate</span>
 
-            <input type="checkbox" />
+            <input type="checkbox" checked={trade} onChange={(e) => setTrade(e.target.checked)}/>
             <p>I trade</p>
-            <input type="text" placeholder='20%'/>
+            <input type="text" placeholder='20%' onChange={(e) => setTradeRate(parseFloat(e.target.value))}/>
 
-            <input type="checkbox" />
+            <input type="checkbox"  checked={income} onChange={(e) => setIncome(e.target.checked)}/>
             <p>I receive income</p>
-            <input type="text" placeholder='20%'/>
+            <input type="text" placeholder='20%' onChange={(e) => setIncomeRate(parseFloat(e.target.value))}/>
 
-            <input type="checkbox" />
+            <input type="checkbox"  checked={donation} onChange={(e) => setDonation(e.target.checked)}/>
             <p>I donate</p>
-            <input type="text" placeholder='10%'/>
+            <input type="text" placeholder='10%' onChange={(e) => setDonationRate(parseFloat(e.target.value))}/>
         </div>
         
         <button onClick={SelectRatesClickHandler}>Continue</button>
